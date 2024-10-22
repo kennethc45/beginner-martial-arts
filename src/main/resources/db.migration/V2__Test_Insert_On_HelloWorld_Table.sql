7b2ff1ee47b348f8c2b7e32fcaf1e4b8959468a2
kennethc45 <kennethcadungog45@gmail.com>
2024-10-21 19:43:58 -0500

    Added sql file to test insert for hello table. Works, still unable to view userRegistration table data

---
+++ src/main/resources/db.migration/V2__Test_Insert_On_HelloWorld_Table.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.HelloWorld (ID, TEXT_TO_DISPLAY)
+VALUES (1, 'Hello, World!');
---
+++ src/main/resources/db.migration/V3__Insert_DefaultUser_On_UserRegistration_Table.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user_registration (ID, username, password, role)
+VALUES (1, 'testUser', 'password1', 'ROLE_ADMIN');
